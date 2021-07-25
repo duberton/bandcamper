265faaacb0339d69183db333388f9156825204ea
Eduardo Berton <duberton@gmail.com>
2021-07-25 17:47:16 -0300

    feat: adding github workflows

--- backend/src/adapter/output/aws/ses/SesEmailNotification.kt
+++ backend/src/adapter/output/aws/ses/SesEmailNotification.kt
@@ -8,18 +8,17 @@ import software.amazon.awssdk.services.ses.model.*
 class SesEmailNotification(private val sesAsyncClient: SesAsyncClient) : EmailNotificationPort {
 
     override fun sendEmail(album: Album) {
-        val sendEmailRequest =
-            SendEmailRequest.builder().source("duberton@gmail.com")
-                .replyToAddresses("duberton@gmail.com")
-                .destination(Destination.builder().toAddresses(album.email).build())
-                .message(
-                    Message.builder()
-                        .subject(Content.builder().charset(Charsets.UTF_8.name()).data("subject").build())
-                        .body(
-                            Body.builder().text(Content.builder().charset(Charsets.UTF_8.name()).data("body").build())
-                                .build()
-                        ).build()
-                ).build()
+        val sendEmailRequest = SendEmailRequest.builder().source("duberton@gmail.com")
+            .replyToAddresses("duberton@gmail.com")
+            .destination(Destination.builder().toAddresses(album.email).build())
+            .message(
+                Message.builder()
+                    .subject(Content.builder().charset(Charsets.UTF_8.name()).data("subject").build())
+                    .body(
+                        Body.builder().text(Content.builder().charset(Charsets.UTF_8.name()).data("body").build())
+                            .build()
+                    ).build()
+            ).build()
         sesAsyncClient.sendEmail(sendEmailRequest)
     }
 }
