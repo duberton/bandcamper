17993121c75e86f5288b9c4328e96730a44f90eb
Eduardo Berton <duberton@gmail.com>
2021-08-01 16:18:33 -0300

    fix: adding aws env vars

--- infrastructure/bandcamper/secrets.sh
+++ infrastructure/bandcamper/secrets.sh
@@ -1,1 +1,2 @@
 kubectl create secret generic google-credentials --from-env-file ../../backend/.googleCredentials
+kubectl create secret generic aws-credentials --from-env-file ../../backend/.awsCredentials
