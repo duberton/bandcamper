8dcc268a7f09ab172d87ee5027ec5e4ee195c70b
Eduardo Berton <duberton@gmail.com>
2021-08-01 17:42:40 -0300

    fix: aws credentials

--- backend/src/adapter/output/aws/ses/config/SesConfig.kt
+++ backend/src/adapter/output/aws/ses/config/SesConfig.kt
@@ -4,6 +4,8 @@ import com.duberton.adapter.output.aws.ses.SesEmailNotification
 import com.duberton.application.port.output.EmailNotificationPort
 import io.ktor.config.*
 import org.koin.dsl.module
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.ses.SesAsyncClient
 import java.net.URI
@@ -16,6 +18,8 @@ fun sesModule(applicationConfig: ApplicationConfig) = module {
 fun buildSesModule(applicationConfig: ApplicationConfig): SesAsyncClient {
     val sesHost = applicationConfig.property("ktor.aws.ses.host").getString()
     val sesPort = applicationConfig.property("ktor.aws.ses.port").getString()
-    return SesAsyncClient.builder().region(Region.US_EAST_1).endpointOverride(URI.create("http://$sesHost:$sesPort"))
+    return SesAsyncClient.builder()
+        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("abc", "abc")))
+        .region(Region.US_EAST_1).endpointOverride(URI.create("http://$sesHost:$sesPort"))
         .build()
 }
